79e5290036ddbcc83f12541f41b11c5453462585
Nuria Rueda <ruedareinanuria@gmail.com>
2022-10-13 10:23:10 +0200

    Ejercicio1 added

---
+++ Ejercicio1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println ("Introduzca el primer numero del que quieres realizar la multiplicacion: ");
+        double numero1 = s.nextDouble();
+        System.out.println("Introduzca el segundo numero: ");
+        double numero2 = s.nextDouble();
+        double total = ( numero1*numero2);
+        System.out.println("La multiplicacion da: " + total); 
+      
+    }
+}
